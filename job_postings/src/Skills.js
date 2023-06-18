d3c92c59366e4b5a58487ea82f6ca20e384e79b9
Moetasimr97 <valencian97@gmail.com>
2023-06-18 10:26:52 +0300

    skills file updated

--- job_postings/src/Skills.js
+++ job_postings/src/Skills.js
@@ -1,42 +1,32 @@
-import React,{useState} from "react"
+import React,{useState,useEffect} from "react"
 import {useRef} from "react"
 export default function Skill({skills,filter_setter,entire_model,ultimate_filterer})
 {
-    const certain_skill=skills
-
-    const [array,setArray]=React.useState([])
     const [clicked,setClickState]=React.useState(false)
-// this is a terrible idea
 
-    const addTag=()=>{
-      setArray((prevState)=> prevState=[...prevState,certain_skill]         
-)
-    }
+    
+    //   end of the body of this function
+    useEffect(()=>{
+        console.log(entire_model)
+    },[clicked])
+
 
     const changeState=()=>{
         setClickState((prevState)=>prevState=true)
+        
         filter_setter()
-        addTag()
-        var updated_model=[];
-        for(var i = 0 ; i< entire_model.length;i++){
-            for(var j=0;entire_model[i].skill.length;j++){
-                if (array[0]==entire_model[i].skill[j])
-                {updated_model.push(entire_model[i])}
-            }
-        }
-
-        ultimate_filterer()
+        // ultimate_filterer()
         
     }
     if(clicked==true){
         
     }
-    console.log(array)
+    
     return (
         <>
         <div className="tag"
         onClick={changeState}>
-            {certain_skill}</div>
+            {skills}</div>
         </>
     )
 }
